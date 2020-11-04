9e32d4454606766a6dea01ae4e0e2f2f6f8db32d
Dima <dima.avanesov@yandex.ru>
2020-11-04 16:50:12 +0700

    Managed to get auth token

--- background.js
+++ background.js
@@ -13,3 +13,38 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 });
+
+const onLoad = () => {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    console.log(token);
+  });
+};
+
+const SPREADSHEET_ID = "1gv49jkuEzNqeUCoIpaC_Xhi47l4VVpsd8OLSJQYgCUg";
+const CLIENT_ID =
+  "830407752509-sbc0rui44t3nmrahtna6etvm1bjq4d81.apps.googleusercontent.com";
+const API_KEY = "AIzaSyB3izcQikDMTg1E4hlMeZx3u0Dy4TMYQdM";
+const SCOPE = "https://www.googleapis.com/auth/spreadsheets";
+
+const initClient = () => {
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      scope: SCOPE,
+      discoveryDocs: [
+        "https://sheets.googleapis.com/$discovery/rest?version=v4",
+      ],
+    })
+    .then(() => {
+      console.log("SUCCESS");
+      updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    })
+    .catch((err) => {
+      console.log("ERROR");
+    });
+};
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  onLoad();
+});
--- content-scripts/popup.js
+++ content-scripts/popup.js
@@ -5,11 +5,5 @@ analyzeButton.onclick = function () {
   const json = JSON.parse(jsonTextarea.value);
   const targetData = json["2"]["6"]["0"]["2"]["1"];
 
-  // alert(json["2"]["6"]["0"]["2"]["1"]["2"]["1"]["3"]);
-
-  console.log(targetData);
-
-  for (let i = 0; i < targetData.length; i++) {
-    console.log(`${targetData[i]["1"]["3"]} - ${targetData[i]["4"]["1"]}`);
-  }
+  chrome.runtime.sendMessage({ data: targetData });
 };
