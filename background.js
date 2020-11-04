5992034d0ba472fb43768c31afaddb9309654027
Dima <dima.avanesov@yandex.ru>
2020-11-04 17:12:38 +0700

    Set token and generated a sheet

--- background.js
+++ background.js
@@ -17,32 +17,42 @@ chrome.runtime.onInstalled.addListener(function () {
 const onLoad = () => {
   chrome.identity.getAuthToken({ interactive: true }, function (token) {
     console.log(token);
+
+    const API_KEY = "AIzaSyB3izcQikDMTg1E4hlMeZx3u0Dy4TMYQdM";
+    const CLIENT_ID =
+      "830407752509-qtgnjs8tde5o0nr1bjh19bvtpv2hn3g1.apps.googleusercontent.com";
+    const DISCOVERY_DOCS = [
+      "https://sheets.googleapis.com/$discovery/rest?version=v4",
+    ];
+    const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+
+    gapi.load("client:auth2", initClient);
+
+    function initClient() {
+      gapi.client
+        .init({
+          apiKey: API_KEY,
+          discoveryDocs: DISCOVERY_DOCS,
+        })
+        .then(function () {
+          console.log("SUCCESS");
+
+          gapi.auth.setToken({
+            access_token: token,
+          });
+
+          gapi.client.sheets.spreadsheets
+            .create({
+              properties: {
+                title: "Generated Sheet",
+              },
+            })
+            .then((response) => {
+              console.log(response);
+            });
+        });
+    }
   });
-};
-
-const SPREADSHEET_ID = "1gv49jkuEzNqeUCoIpaC_Xhi47l4VVpsd8OLSJQYgCUg";
-const CLIENT_ID =
-  "830407752509-sbc0rui44t3nmrahtna6etvm1bjq4d81.apps.googleusercontent.com";
-const API_KEY = "AIzaSyB3izcQikDMTg1E4hlMeZx3u0Dy4TMYQdM";
-const SCOPE = "https://www.googleapis.com/auth/spreadsheets";
-
-const initClient = () => {
-  gapi.client
-    .init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      scope: SCOPE,
-      discoveryDocs: [
-        "https://sheets.googleapis.com/$discovery/rest?version=v4",
-      ],
-    })
-    .then(() => {
-      console.log("SUCCESS");
-      updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    })
-    .catch((err) => {
-      console.log("ERROR");
-    });
 };
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
