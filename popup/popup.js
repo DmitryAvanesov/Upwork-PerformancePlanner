f4359ed9ae4f09f3453365e07572af77e4c64b14
Dima <dima.avanesov@yandex.ru>
2020-11-20 16:10:36 +0700

    Material designed popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,19 @@
-const transformButton = document.querySelector(".transform-button");
-const textarea = document.querySelector(".textarea");
-const transformChartButton = document.querySelector(".transform-chart-button");
+// const transformButton = document.querySelector(".transform-button");
+// const textarea = document.querySelector(".textarea");
+// const transformChartButton = document.querySelector(".transform-chart-button");
 
-transformChartButton.addEventListener("click", function () {
-  chrome.runtime.sendMessage({ message: "TRANSFORM_INTERCEPTED_JSON" });
-});
+// transformChartButton.addEventListener("click", function () {
+//   chrome.runtime.sendMessage({ message: "TRANSFORM_INTERCEPTED_JSON" });
+// });
 
-transformButton.addEventListener("click", function () {
-  chrome.runtime.sendMessage({
-    message: "TRANSFORM_JSON",
-    data: JSON.parse(textarea.value),
-  });
-});
+// transformButton.addEventListener("click", function () {
+//   chrome.runtime.sendMessage({
+//     message: "TRANSFORM_JSON",
+//     data: JSON.parse(textarea.value),
+//   });
+// });
+
+mdc.textField.MDCTextField.attachTo(document.querySelector(".plan-name"));
+mdc.select.MDCSelect.attachTo(document.querySelector(".main-metric"));
+mdc.select.MDCSelect.attachTo(document.querySelector(".date-range"));
+mdc.ripple.MDCRipple.attachTo(document.querySelector(".build-plan"));
